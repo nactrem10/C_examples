850d353514e5bd2f7eb9cc906106f3db8108e4a8
mertcankirtay <kirtay.mertcan@gmail.com>
2020-02-13 21:35:36 +0300

    comments added

--- int.c
+++ int.c
@@ -2,17 +2,19 @@
 
 int main(){
 
+    // Declaring integers to use
     int array[20];
 
+    // Finding length of an array
     int length = sizeof(array) / sizeof(array[0]);
-    
+
+    // Filling and printing an array at the same time
     for (int i = 0; i < length; i++){
         
         array[i] = i;
 
         printf("The %d th elemenet of the array is %d\n", i, array[i]);
     }
-    
 
     return 0;
 }
--- string.c
+++ string.c
@@ -3,19 +3,22 @@
 
 int main(){
 
+    // Declaring strings to use
     char input[100], toReverse[100], reversed[100];
     
+    // Finding length of a string
     printf("Enter a string: \n");
     scanf("%s", input);
     printf("Length of %s is: %ld\n", input, strlen(input));
 
+    // Reversing a string
     printf("Input a string to reverse: \n");
     scanf("%s", toReverse);
 
     int begin = 0, end = strlen(toReverse) -1;
 
     for (begin = 0; begin < strlen(toReverse); begin++) {
-        
+
         reversed[begin] = toReverse[end];
         end--;
     }
