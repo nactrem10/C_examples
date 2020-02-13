96fd6ff3a70bd3ec823e7ddbefec38167ba4ad7a
mertcankirtay <kirtay.mertcan@gmail.com>
2020-02-13 21:32:02 +0300

    reverse string implemented

--- string.c
+++ string.c
@@ -3,11 +3,26 @@
 
 int main(){
 
-    char input[100];
+    char input[100], toReverse[100], reversed[100];
     
     printf("Enter a string: \n");
     scanf("%s", input);
-    printf("Length of %s is: %ld\n",input, strlen(input));
+    printf("Length of %s is: %ld\n", input, strlen(input));
+
+    printf("Input a string to reverse: \n");
+    scanf("%s", toReverse);
+
+    int begin = 0, end = strlen(toReverse) -1;
+
+    for (begin = 0; begin < strlen(toReverse); begin++) {
+        
+        reversed[begin] = toReverse[end];
+        end--;
+    }
+
+    reversed[begin] = '\0';
+
+    printf("Reverse of %s is: %s\n", toReverse, reversed);
 
     return 0;
 }
