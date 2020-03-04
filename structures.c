fe448d97b9facf6da52d0b4c2aca3407f088d782
mertcankirtay <kirtay.mertcan@gmail.com>
2020-03-04 14:00:25 +0300

    person objects are implemented

--- structures.c
+++ structures.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-int main(){
+struct Person {
+    int id;
+    char *name;
+    int age;
+    struct Person *spouse;
+} firstPerson, secondPerson;
 
+int main(){
     
+    firstPerson.id = 123;
+    firstPerson.name = "Bob";
+    firstPerson.age = 25;
+
+    secondPerson.id = 132;
+    secondPerson.name = "Alice";
+    secondPerson.age = 25;
+    secondPerson.spouse = &firstPerson;
+
+    firstPerson.spouse = &secondPerson;
+
+    // Printing first persons variables
+    printf("Id: %d, Name: %s, Age: %d, Name of the spouse: %s\n", firstPerson.id, firstPerson.name, firstPerson.age, firstPerson.spouse->name);
 
     return 0;
 }
