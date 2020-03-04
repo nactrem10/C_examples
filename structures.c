a4acbbf1882f915fde9a3a6facd453a396040645
mertcankirtay <kirtay.mertcan@gmail.com>
2020-03-04 14:27:47 +0300

    array of structs implemented

--- structures.c
+++ structures.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Creating the Person class
 struct Person {
     int id;
     char *name;
@@ -21,7 +22,22 @@ int main(){
     firstPerson.spouse = &secondPerson;
 
     // Printing first persons variables
-    printf("Id: %d, Name: %s, Age: %d, Name of the spouse: %s\n", firstPerson.id, firstPerson.name, firstPerson.age, firstPerson.spouse->name);
+    printf("Id: %d, Name: %s, Age: %d, Name of the spouse: %s\n\n", firstPerson.id, firstPerson.name, firstPerson.age, firstPerson.spouse->name);
+
+    // Creating an array of persons and filling the array
+    struct Person city[2];
+
+    city[0] = firstPerson;
+    city[1] = secondPerson;
+    
+    // Printing the array
+    printf("City array; \n");
+    
+    for (int i = 0; i < sizeof(city) / sizeof(city[0]); i++) {
+
+        printf("Id: %d, Name: %s, Age: %d, Name of the spouse: %s\n", city[i].id, city[i].name, city[i].age, city[i].spouse->name);
+    }
+    
 
     return 0;
 }
