32e40b6bbae21d38ae22fdeed79be459007e9572
mertcankirtay <kirtay.mertcan@gmail.com>
2020-03-04 13:15:27 +0300

    calculating avarage implemented

--- int.c
+++ int.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+
+// Method for calculating avarage of two integers
+int calculateAvarage(int a, int b) {
+
+    return ((a+b) / 2);
+}
+
 int main(){
 
     // Declaring integers to use
@@ -26,6 +33,16 @@ int main(){
 
     // Printing two integers after swap
     printf("After swap;\nFirst: %d, Second: %d\n", first, second);
+
+    // Getting two numbers from user for calculating avarage
+    printf("Enter first number to calculate avarage: \n");
+    scanf("%d", &first);
+
+    printf("Enter second number to calculate avarage: \n");
+    scanf("%d", &second);
+
+    // Printing the avarage of inputs
+    printf("The avarage of %d and %d is: %d\n", first, second, calculateAvarage(first, second));
 
     return 0;
 }
