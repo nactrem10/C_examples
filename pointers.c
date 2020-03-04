a3f89668419b89df3214862b910d12d4aed16e33
mertcankirtay <kirtay.mertcan@gmail.com>
2020-03-04 12:11:50 +0300

    printing addresses implemented

--- pointers.c
+++ pointers.c
@@ -2,7 +2,19 @@
 
 int main(){
 
-    
+    // Declaring the array
+    int array[5];
+
+    // Finding length of an array
+    int length = sizeof(array) / sizeof(array[0]);
+
+    // Filling and printing addresses of an array at the same time 
+    for (int i = 0; i < length; i++){
+        
+        array[i] = i;
+
+        printf("The %d th elemenets address of the array is %p\n", i, &array[i]);
+    }
 
     return 0;
 }
