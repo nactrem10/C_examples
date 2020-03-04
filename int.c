395669a232a00386c90327e910c24a11c8b60a28
mertcankirtay <kirtay.mertcan@gmail.com>
2020-03-04 12:40:11 +0300

    swapping two integers implemented

--- int.c
+++ int.c
@@ -3,7 +3,7 @@
 int main(){
 
     // Declaring integers to use
-    int array[20];
+    int array[20], first = 5, second = 2;;
 
     // Finding length of an array
     int length = sizeof(array) / sizeof(array[0]);
@@ -15,6 +15,17 @@ int main(){
 
         printf("The %d th elemenet of the array is %d\n", i, array[i]);
     }
+
+    // Printing two integers before swap
+    printf("Before swap;\nFirst: %d, Second: %d\n", first, second);
+
+    // Swapping the integers
+    first = first + second;
+    second = first - second;
+    first = first - second;
+
+    // Printing two integers after swap
+    printf("After swap;\nFirst: %d, Second: %d\n", first, second);
 
     return 0;
 }
